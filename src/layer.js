b3dacf467b10960b7c7fdef646f865a68738dd6e
Michał Żaloudik <ponury.kostek@gmail.com>
2018-05-14 15:01:51 +0200

    final class `Layer`

--- src/layer.js
+++ src/layer.js
@@ -7,112 +7,127 @@
  *
  * @constructor
  */
-function Layer() {
-	/**
-	 *
-	 * @type {Layer|null}
-	 */
-	this.next = null;
-	this.clearStats();
-}
-
-Layer.prototype._next = function (err, name, args, callback) {
-};
-/**
- *
- * @param {string} key
- * @param {string} field
- * @param {function} callback
- */
-Layer.prototype.has = function (key, field, callback) {
-	this._has(key, field, (err, value) => {
-		if (err || value === null) {
-			return callback(null, false);
-		}
-		callback(null, true);
-	});
-};
-/**
- *
- * @param {string} key
- * @param {string} field
- * @param {function} callback
- */
-Layer.prototype.get = function (key, field, callback) {
-	this._get(key, field, (err, value) => {
-		if (!err && value !== null) {
-			this.stats.hits++;
-			return callback(null, value);
-		}
-		this.stats.misses++;
-		this._next(err, "get", [
-			key,
-			field
-		], (err, value) => {
-			this._set(key, field, value, () => {
-				callback(null, value);
-			});
-		});
-	});
-};
-/**
- *
- * @param {string} key
- * @param {string} field
- * @param {*} value
- * @param {function} callback
- */
-Layer.prototype.set = function (key, field, value, callback) {
-	this._set(key, field, value, (err) => {
-		this._next(err, "set", [
-			key,
-			field,
-			value
-		], callback);
-	});
-};
-/**
- *
- * @param {string} key
- * @param {string} field
- * @param {function} callback
- */
-Layer.prototype.delete = function (key, field, callback) {
-	this._delete(key, field, (err) => {
-		this._next(err, "set", [
-			key,
-			field,
-			value
-		], callback);
-	});
-};
-/**
- * @param {function} callback
- */
-Layer.prototype.clear = function (callback) {
-	this._clear((err) => {
-		this._next(err, "set", [], callback);
-	});
-};
-/**
- * @param [clear]
- * @returns {{hits: number, misses: number}}
- */
-Layer.prototype.getStats = function (clear) {
-	const stats = this.stats;
-	stats.ratio = stats.hits && stats.misses ? stats.hits / stats.misses : 0;
-	if (clear) {
+class Layer {
+	constructor() {
+		/**
+		 *
+		 * @type {Layer|null}
+		 */
+		this.next = null;
 		this.clearStats();
 	}
-	return stats;
-};
-/**
- *
- */
-Layer.prototype.clearStats = function () {
-	this.stats = {
-		hits: 0,
-		misses: 0
+
+	_next(err, name, args, callback) {
+		const next = this.next;
+		if (next !== null) {
+			args.push(callback);
+			return next[name].apply(next, args);
+		}
+		callback(err, null);
 	};
-};
+
+	/**
+	 *
+	 * @param {string} key
+	 * @param {string} field
+	 * @param {function} callback
+	 */
+	has(key, field, callback) {
+		this._has(key, field, (err, value) => {
+			if (err || value === null) {
+				return callback(null, false);
+			}
+			callback(null, true);
+		});
+	};
+
+	/**
+	 *
+	 * @param {string} key
+	 * @param {string} field
+	 * @param {function} callback
+	 */
+	get(key, field, callback) {
+		this._get(key, field, (err, value) => {
+			if (!err && value !== null) {
+				this.stats.hits++;
+				return callback(null, value);
+			}
+			this.stats.misses++;
+			this._next(err, "get", [
+				key,
+				field
+			], (err, value) => {
+				this._set(key, field, value, () => {
+					callback(null, value);
+				});
+			});
+		});
+	};
+
+	/**
+	 *
+	 * @param {string} key
+	 * @param {string} field
+	 * @param {*} value
+	 * @param {function} callback
+	 */
+	set(key, field, value, callback) {
+		this._set(key, field, value, (err) => {
+			this._next(err, "set", [
+				key,
+				field,
+				value
+			], callback);
+		});
+	};
+
+	/**
+	 *
+	 * @param {string} key
+	 * @param {string} field
+	 * @param {function} callback
+	 */
+	delete(key, field, callback) {
+		this._delete(key, field, (err) => {
+			this._next(err, "delete", [
+				key,
+				field
+			], callback);
+		});
+	};
+
+	/**
+	 * @param {function} callback
+	 */
+	clear(callback) {
+		this._clear((err) => {
+			this._next(err, "clear", [], callback);
+		});
+	};
+
+	/**
+	 * @param [clear]
+	 * @returns {{hits: number, misses: number}}
+	 */
+	getStats(clear) {
+		const stats = this.stats;
+		stats.ratio = stats.hits && stats.misses ? stats.hits / stats.misses : 0;
+		if (clear) {
+			this.clearStats();
+		}
+		return stats;
+	};
+
+	/**
+	 *
+	 */
+	clearStats() {
+		this.stats = {
+			hits: 0,
+			misses: 0
+		};
+	};
+}
+
 module.exports = Layer;
